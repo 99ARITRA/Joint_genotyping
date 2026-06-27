a3e5ae16e7baa24137e2bfa16fc3688b94e65242
99ARITRA <palodhiaritra_9961@outlook.com>
2026-06-27 12:40:36 +0530

    Comments added

--- merge_tables.py
+++ merge_tables.py
@@ -1,5 +1,9 @@
+# ----------------------------------------------------- #
+# MERGE THE GENOTYPE AND SNPEFF TABLES
+# ----------------------------------------------------- #
 import pandas as pd
 import sys
+
 def merge_tables(tab1, tab2, otab):    
     tab1= pd.read_csv(tab1, sep= "\t", low_memory= False, on_bad_lines= "skip")
     tab2= pd.read_csv(tab2, sep= "\t", low_memory= False, on_bad_lines= "skip")
@@ -8,7 +12,7 @@ def merge_tables(tab1, tab2, otab):
                 "ANN[*].EFFECT": "EFFECT", "ANN[*].IMPACT": "IMPACT", "ANN[*].GENE": "GENE", 
                 "ANN[*].GENEID": "GENEID", "ANN[*].FEATURE": "FEATURE", 
                 "ANN[*].FEATUREID": "FEATUREID", "ANN[*].HGVS_C": "CHROMOSOME_HGVS", "ANN[*].HGVS_P": "PROTEIN_HGVS",
-                "CLNSIG": "CLINICAL_SIGNIFICANCE", "CLNHGVS": "CLINVAR_HGVS", "AF_EXAC": "EXAC_ALLELE_FREQUENCY", "ORIGIN": "VARIANT_TYPE" }
+                "CLNSIG": "CLINICAL_SIGNIFICANCE", "CLNHGVS": "CLINVAR_HGVS", "AF_EXAC": "EXAC_ALLELE_FREQUENCY"}
     mtab= mtab.fillna("-")
     mtab.rename(columns= new_cols, inplace= True)
     mtab.to_csv(otab, sep= "\t", index= False)
@@ -16,4 +20,5 @@ def merge_tables(tab1, tab2, otab):
 tab1= sys.argv[1]
 tab2= sys.argv[2]
 otab= sys.argv[3]
+
 merge_tables(tab1, tab2, otab)
